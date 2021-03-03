c1527a4c38ad8bba6fc6fc7ad7ad095b381607ed
Andre Lucas <andre.lucas284@gmail.com>
2021-03-03 17:35:50 -0500

    made click event work properly and started generate password function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//1 make click event/button work
+//2. ask user how long of a password
+//3. ask user what characters to include
+//4. take information and use function to build password
+//5. display password
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -10,5 +16,11 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  console.log("generatePassword");
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//console.log("The page is connected")
